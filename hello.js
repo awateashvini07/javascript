9e3e5bff7bc19a6dea6d86617492704f7b3e1587
awateashvini07 <awateashvini07@gmail.com>
2023-05-05 08:53:46 +0530

    hello words
    hello words

---
+++ hello.js
@@ -0,0 +1,21 @@
+
+
+
+
+console.log(" Ashwini Awate...");
+console.log(" My Address : At post  chatrapati shivaji Nagar patur,Akola");
+
+var firstName =" Ashwini";
+console.log( firstName);
+
+var age; // Variable Declaration 
+age = "22";  // Variable Initialization
+console.log(age);
+
+var holder; // Variable Declaration
+console.log(holder); // undefined
+holder = "My Spec"; // Variable Initialization
+console.log(holder);
+
+holder = "Sachin Spec"; // Variable update
+console.log(holder); //
