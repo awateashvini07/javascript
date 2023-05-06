3192742a080f3b441dad8d275e8ea2351d17f1bd
awateashvini07 <awateashvini07@gmail.com>
2023-05-06 23:19:49 +0530

    creating variables in JS
    creating variables in JS

---
+++ 01-variable-assign.js
@@ -0,0 +1,34 @@
+var myfullName;
+console.log(myfullName);
+myfullName = " Ashwini Awate";
+console.log(myfullName);
+myfullName = " Ashwini Sanjay Awate";
+console.log(myfullName);
+
+var myLove;
+myLove = "myLove";
+console.log(myLove);
+myLove = " i love only JS";
+console.log(myLove);
+
+var firstName;
+firstName = " Ashwini";
+console.log(firstName);
+var lastName;
+lastName = "Awate";
+console.log(lastName);
+var age;
+age = "22";
+console.log(age);
+var address;
+address = " at post chatrapati shivaji Nagar patur,Akola";
+console.log(address);
+var pinCode;
+pinCode = " 444501";
+console.log(pinCode);
+var gender;
+gender = " Female";
+console.log(gender);
+var isMarried;
+isMarried = "No";
+console.log(isMarried);
--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@
 
 
 console.log(" Ashwini Awate...");
-console.log(" My Address : At post  chatrapati shivaji Nagar patur,Akola");
+console.log(" My Address : at post  chatrapati shivaji Nagar patur,AkolAa");
 
 var firstName =" Ashwini";
 console.log( firstName);
@@ -19,3 +19,39 @@ console.log(holder);
 
 holder = "Sachin Spec"; // Variable update
 console.log(holder); //
+
+var myfullName;
+console.log( myfullName);
+myfullName=" Ashwini Awate"
+console.log( myfullName);
+myfullName=" Ashwini Sanjay Awate"
+console.log( myfullName);
+
+var myLove;
+myLove="myLove";
+console.log( myLove);
+myLove=" i love only JS";
+console.log( myLove);
+
+var firstName
+firstName=" Ashwini";
+console.log( firstName);
+var lastName
+lastName="Awate";
+console.log(lastName);
+var age;  
+age = "22";  
+console.log(age);
+var address;
+address = " at post chatrapati shivaji Nagar patur,Akola";  
+console.log(address);
+var pinCode;
+pinCode=" 444501";
+console.log( pinCode);
+var gender;
+gender=" Female"
+console.log(gender);
+var isMarried;
+isMarried="No"
+console.log( isMarried);
+
