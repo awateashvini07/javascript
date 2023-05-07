6d517669a88222d14bc230092b71ca5c33564165
awateashvini07 <awateashvini07@gmail.com>
2023-05-07 23:49:45 +0530

    types of  variables and data types
    types of  variables and data types

---
+++ 03_typeOfvariables.js
@@ -0,0 +1,36 @@
+var bankName;
+console.log( bankName);
+bankName=" SBI"
+console.log( bankName);
+var bankName= "SBI";
+var typeOfbankName = typeof bankName;
+console.log( "bankName-> SBI :",typeOfbankName );
+
+var marks;
+console.log( marks);
+marks=90;
+console.log( marks);
+var marks=90;
+console.log( "marks:", 90);
+var typeofmarks= typeof marks;
+console.log( "marks-> 90:", typeofmarks);
+
+var isworking;
+console.log( isworking);
+var isworking=true;
+console.log( "isworking:", true);
+var typeofisworking=typeof isworking;
+console.log( " isworking -> true:", typeofisworking);
+
+var totalCount;
+totalCount=" Hundred and Seven";
+console.log( totalCount);
+ var totalCount="Hundred and Seven";
+ var typeoftotalCount= typeof totalCount;
+ console.log( " totalCount->Hundred and Seven:",typeoftotalCount);
+ 
+var totalCount=" 107";
+console.log( " totalCount:",107);
+typeoftotalCount=typeof 107; 
+console.log( " totalCount-> 107 :", typeoftotalCount);
+
