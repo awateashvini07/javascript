54cbc326b448d5aab02ee7d9476323a488b8b6a5
awateashvini07 <awateashvini07@gmail.com>
2023-05-07 22:41:50 +0530

    create 02-variable basics assignment
    create 02-variable basics assignment

---
+++ 02-variableBasicsAssig.js
@@ -0,0 +1,70 @@
+//1.String
+var firstName;
+firstName="kiran";
+console.log( firstName);
+var firstName = "kiran"; 
+var typeOfFirstName = typeof firstName;
+console.log( "firstName -> kiran :", typeOfFirstName );
+var hobby;
+hobby = "reading Books "
+console.log( hobby);
+var hobby = "reading Books"; 
+var typeOfhobby = typeof hobby;
+console.log( "hobby -> reading books: ", typeOfhobby );
+
+var Education;
+ Education= "BE "
+console.log( Education);
+var Education = "BE"; 
+var typeOfEducation = typeof Education;
+console.log( "Education -> BE: ", typeOfEducation );
+//2. Number
+var marks;
+weight = 55;
+console.log( weight);
+var weight=55;
+console.log( " weight:",weight, );
+var typeOfweight = typeof weight;
+console.log( "weight -> 55: ", typeOfweight );
+
+var age;
+age = 23;
+console.log( age);
+var age=23;
+console.log( " age:",23, );
+var typeOfage = typeof age;
+console.log( "age -> 23: ", typeOfweight );
+
+var pinCode;
+pinCode=444501;
+console.log( pinCode);
+var pinCode=444501;
+console.log( "pinCode:", 444501);
+var typeofpinCode= typeof pinCode;
+console.log( "pincode-> 444501:", typeofpinCode); 
+
+//Boolean
+var knowAngular = true; 
+var typeofknowAngular=typeof knowAngular;
+console.log( " knowAngular -> true:", typeofknowAngular);
+
+var isSoftwareEngineer = true;
+var isSoftwareEngineer=typeof isSoftwareEngineer;
+console.log( " isSoftwareEngineer -> true:",isSoftwareEngineer);
+
+ var isDancer = false;
+ var typeofisDancer = typeof isDancer;
+console.log( " isDancer -> false:",typeofisDancer);
+
+var diffrence ;
+diffrence=" **Diffrence between Undefined Vs Null**"
+console.log( diffrence);
+  var undefined;
+  undefined=" Undefine =  When a variable is declare and not assigned any value then it is undefined";
+  console.log( undefined);
+
+  var Null;
+  Null=" Null =  a null value means that there is no value,its is simply nothing"
+  console.log( Null);
+
+
