cb06fd660563b3f56e89478081fbce261755b275
awateashvini07 <awateashvini07@gmail.com>
2023-05-12 01:16:32 +0530

    creating  swap variables assig
    creating  swap variables assig

---
+++ 02_swapVariablesAssig.js
@@ -0,0 +1,22 @@
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log(" Before Swap..", " sweety:", sweety, " cutie:", cutie);
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log(" After Swap..", " sweety:", sweety, " cutie:", cutie);
+
+var swapNumber;
+swapNumber=" -----Swap Number-----"
+console.log( swapNumber);
+
+var 
+num1="100";
+num2="200";
+num3= "300";
+console.log("Before Swap..", "num1 : ", num1, " num2 : ", num2, " num3 :", num3);
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("After Swap..", "num1 : ", num1, " num2 : ", num2, " num3 :", num3);
