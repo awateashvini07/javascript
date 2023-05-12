da21a285d7b5ce3dce0334cbd110991ddd9e8975
awateashvini07 <awateashvini07@gmail.com>
2023-05-13 01:02:01 +0530

    creating string assignment
    creating string assignment

---
+++ 04_stringBasicAssignment.js
@@ -0,0 +1,28 @@
+function stringBasics(params) {
+  var myDreamCompany = " Oracle";
+  console.log(" My Dream compnay is:", myDreamCompany);
+}
+stringBasics();
+console.log( " ------------------------------------------------------");
+
+var hobby1 = " Reading Books";
+var hobby2 = " Gardening";
+var hobby3 = " Learning languages";
+
+var myHobbies = hobby1.concat(hobby2, hobby3);
+console.log(" My Hobbies are :", myHobbies);
+var myHobbieslength = myHobbies.length;
+console.log(" total Number of char : ", myHobbieslength);
+
+var hobby1 = "Reading Books";
+var hobby1length = hobby1.length;
+console.log(" Hobby 1 char -> Reading Books:", hobby1length);
+
+var hobby2 = "Gardening";
+var hobby2length = hobby2.length;
+console.log(" Hobby 2 char -> Gardening:", hobby2length);
+
+var hobby3 = "Learning languages";
+var hobby3length = hobby3.length;
+console.log(" Hobby 3 char -> Learning languages:", hobby3length);
+console.log( " ------------------------------------------------------");
