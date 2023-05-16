45d4bc3de6536c79364169a1128ab320112cf1a5
awateashvini07 <awateashvini07@gmail.com>
2023-05-16 23:48:45 +0530

    operator ternary assignment completed
    operator ternary assignment completed

---
+++ 05_operatorTernaryAssigC.js
@@ -0,0 +1,43 @@
+console.log ( " *Male marriage Eligibility" );
+var gender= "male";
+var age=25
+var boyName= "Billgates"
+var result =  ( age>=21) ?` Hey ${boyName}  you are eligible for marriage` : ` Hey ${boyName}  you are not eligible for marriage`;
+console.log(`${result}`);
+
+var gender= "male";
+var age=17
+var boyName= "Stew Jobs"
+var result =  ( age>=21) ?` Hey ${boyName}  you are eligible for marriage` :` Hey ${boyName}  you are not eligible for Marriage`;
+console.log(`${result}`);
+
+ console.log( " --------------------------------------------------");   
+    
+
+console.log ( " * female marriage Eligibility" );
+var gender= "female";
+var age=16
+var girlName= "Jenifer"
+var result =  ( age>= 18) ?` Hey ${girlName}  you are eligible for marriage` : ` Hey ${girlName}  you are not eligible for marriage`;
+console.log(`${result}`);
+
+var gender= "female";
+var age=27
+var girlName = "Malinda Gates"
+var result =  ( age>=18) ?` Hey ${girlName}  you are eligible for marriage` :` Hey ${girlName}  you are not eligible for Marriage`;
+console.log(`${result}`);
+
+console.log( " --------------------------------------------------");   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
