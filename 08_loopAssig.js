2e61375988cc94183ed365bd3577f09042835576
awateashvini07 <awateashvini07@gmail.com>
2023-05-20 19:51:13 +0530

    loop assign based on vowels ,odd post char
    loop assign based on vowels ,odd post char

---
+++ 08_loopAssig.js
@@ -0,0 +1,86 @@
+var word = "I am very good IT Developer";
+console.log(` string : ${word}`);
+// a e i o u == A E I O U
+var count = 0;
+for (let index = 0; index < word.length; index++) {
+  var char = word.charAt(index); //
+  if (
+    char == "a" ||
+    char == "e" ||
+    char == "i" ||
+    char == "o" ||
+    char == "u" ||
+    char == "A" ||
+    char == "E" ||
+    char == "I" ||
+    char == "O" ||
+    char == "U"
+  ) {
+    console.log(char);
+    count++;
+  }
+}
+console.log(` * Count total vowels in a string is "${word}": ${count}`);
+console.log(" ---------------------------------------------------------------");
+console.log('----* "cube of a Number"----');
+
+function cubeOfsum(num) {
+  var cubesum = num * num * num;
+  console.log(` Cub  of Number ${num} : ${cubesum}`);
+  for (let index = 1; index < 5; index++) {
+    console.log();
+  }
+}
+
+cubeOfsum(1);
+cubeOfsum(2);
+cubeOfsum(3);
+cubeOfsum(4);
+cubeOfsum(5);
+
+function sumcubeOf(num1, num2, num3, num4, num5) {
+  var cubesum =
+    num1 * num1 * num1 +
+    num2 * num2 * num2 +
+    num3 * num3 * num3 +
+    num4 * num4 * num4 +
+    num5 * num5 * num5;
+  console.log(
+    `  sum of Cub of a given  Number :${num1},${num2},${num3},${num4},${num5} is: ${cubesum}`
+  );
+  for (let index = 1; index < 5; index++) {
+    console.log();
+  }
+}
+sumcubeOf(1, 2, 3, 4, 5);
+
+console.log("-----------------------------------");
+// to print the characters in the odd index position
+var word = "Hard work always pays back";
+console.log(` * string : "${word}"`);
+var Count=0;
+var string= "";
+for (let index = 0; index <= word.length; index++) {
+  var char = word.charAt(index); //
+  if (index % 2==!0) {
+    string= string.concat(char) .trim( )
+    
+  }
+}
+console.log( `    Odd positioned chars  :${string}`);
+console.log("--------------------------------------------------");
+
+var word = "Soon I will be Angular IT Champ";
+console.log(` * string : "${word}"`);
+var Count=0;
+var string= "";
+for (let index = 0; index <= word.length; index++) {
+  var char = word.charAt(index); //
+  if (index % 2==!0) {
+    string= string.concat(char) .trim( )
+    
+  }
+}
+console.log( `    Odd positioned chars  :${string}`);
+console.log("----------------------------------------");
+
