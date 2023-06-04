bdb2a43c08ca7e5313d4e0ff20023453b61395c3
awateashvini07 <awateashvini07@gmail.com>
2023-06-04 21:22:54 +0530

    while loop base assignment
    while loop base assignment

---
+++ 08_whileLoopAssignment01.js
@@ -0,0 +1,65 @@
+// WAP to print number from 5 to 15
+console.log(`1. Numbers from 5 to  15  incrementing  by 1 ` );
+
+let i=5; // initialization i = 5 6 7
+while (i<=15) { // condition 0<=15
+   console.log(i); // 0  1 2
+   i = i + 1;
+}
+console.log(`----------------------------------------------------------------------------------------`);
+
+console.log(`2. Numbers from 50 to 40  decrementing  by 1 ` );
+let k=50;
+while( k>=40){
+    console.log( k);
+    k=k-1;
+}
+console.log(`----------------------------------------------------------------------------------------`);
+console.log(`3.  First 15 odd Numbers ` );
+let m=0;
+while( m<=15){
+if (m%2!==0) {
+   console.log(m);
+   
+}
+m++
+}
+console.log(`----------------------------------------------------------------------------------------`);
+console.log(`4. First 10 even Numbers ` );
+
+let j=0;  // initialization
+while (j<=10) {  // condition
+   if (j%2==0) {
+      console.log(j);
+   }
+   j++; // update expression
+}
+console.log(`----------------------------------------------------------------------------------------`);
+console.log(`5. tabel of 5 ` );
+
+let n=0; 
+while (n<50) { 
+ if  (n = n + 5){
+   console.log(n); 
+}
+}
+console.log(`----------------------------------------------------------------------------------------`);
+console.log(`6. tabel of 10` );
+
+let a=1; 
+while (a<=100) { 
+ if  (a%10==0 ){
+   console.log(a); 
+}
+a++
+}
+console.log(`----------------------------------------------------------------------------------------`);
+console.log(`7. Revers order tabel  of  10  ` );
+let r=100; 
+while (r>=10) { 
+ if  (r%10==0 ){
+   console.log(r); 
+}
+r--
+}
+console.log(`----------------------------------------------------------------------------------------`);
