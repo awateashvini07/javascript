82e38225773553af0ed8d83b2dfee3f883df2892
awateashvini07 <awateashvini07@gmail.com>
2023-06-04 21:24:16 +0530

    assignment base on object
    assignment base on object

---
+++ 11_object_assigD.js
@@ -0,0 +1,21 @@
+ let professor= {
+    name: "Kiran",
+    age:  30,
+    city: " Mumbai",
+    isMarride: true,
+    height: "5feet",
+    education: "Phd in  control system",
+    degree :{
+      PHD: " control system",
+      mTech: "power system",
+      engineering: " Electrical engineering",
+       diploma :" Electrical ",
+    } 
+   
+ };
+ console.log( professor);
+
+ var  certificates=["Hacker Rank Participation", 'Certificate in IFE course', "Certificates in ADV programing"]
+ console.log( certificates);
+
+ console.log( professor,certificates);
